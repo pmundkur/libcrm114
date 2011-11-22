71f1a3472a9eb1406b05a8095121685b02f68afb
pmundkur <prashanth.mundkur@gmail.com>
2011-11-21 19:20:33 -0800

    Add some license lines to Python module.

--- python/pycrm114_module.c
+++ python/pycrm114_module.c
@@ -1,3 +1,20 @@
+// Copyright 2011 Prashanth Mundkur.
+//
+//   This file is part of the CRM114 Library.
+//
+//   The CRM114 Library is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Lesser General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   The CRM114 Library is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Lesser General Public License for more details.
+//
+//   You should have received a copy of the GNU Lesser General Public License
+//   along with the CRM114 Library.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <Python.h>
 #include <assert.h>
 #include <string.h>
