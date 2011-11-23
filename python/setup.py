238bb432dbbf3e3a0cdabd3e02e9e499fd3f827d
Prashanth Mundkur <prashanth.mundkur@nokia.com>
2011-11-23 11:35:41 -0800

    Remove hardcoded paths in setup; make setup a little more complete.

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,24 @@
 import sys, os
 from distutils.core import setup, Extension
 
+script_dir = os.path.dirname(os.path.realpath( __file__ ))
+top_dir = os.path.dirname(script_dir)
+inc_dir = os.path.join(top_dir, "include")
+lib_dir = os.path.join(top_dir, "lib")
+
 pycrm114_module = Extension('pycrm114',
                             sources = ['pycrm114_module.c'],
-                            include_dirs = ['../include'],
-                            library_dirs = ['/home/mundkur/proj/libcrm114/lib'],
-                            runtime_library_dirs = ['/home/mundkur/proj/libcrm114/lib'],
+                            include_dirs = [inc_dir],
+                            library_dirs = [lib_dir],
+                            runtime_library_dirs = [lib_dir],
                             libraries = ['tre', 'crm114'],
                             extra_compile_args=['-Wall', '-g'])
 
 setup(name = 'pycrm114',
-      version = '0.1',
+      version = '0.1.0',
       description = 'Python interface to libcrm114',
+      author = 'Prashanth Mundkur',
+      author_email = 'prashanth.mundkur at gmail.com',
+      url = 'https://github.com/pmundkur/libcrm114',
+      license = 'LGPL',
       ext_modules = [pycrm114_module])
